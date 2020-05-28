c1ab0ec7cd4c378a8dad7e189eb1f7770c1c45e4
Guilherme Tomaz Brito <gtbrito@server01.foursys.local>
2020-05-28 15:17:46 -0300

    Codigo com namespace

---
+++ app/ts/views/MensagemView.ts
@@ -0,0 +1,10 @@
+namespace Views{
+    export class MensagemView extends Views.View<string>{
+
+        template(model: string): string {
+         
+            return `<p class="alert alert-info">${model}</p>`
+        }
+        
+        }
+}
